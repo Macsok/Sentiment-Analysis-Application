1b7c082caf6deff15e4a3c4af0f924b5d28a1af7
Macsok <115928066+Macsok@users.noreply.github.com>
2024-11-05 23:07:23 +0100

    updated text analyse, added skip and translate parameters

--- scripts/analyses.py
+++ scripts/analyses.py
@@ -4,8 +4,11 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-def analyseText(text : str = ''):
-    """Analyses sentiment of a provided text. ..."""
+def analyseText(text : str = '', translate : bool = True, skip_non_eng : bool = False):
+    """Analyses sentiment of a provided text. Translates every provided
+    data by default, to enhance performance you may set 'translate' to False.
+    Returns 1 if language not found, returns 2 if language translation failed.
+    Returns 0 for every text that wasn't english and was skipped."""
     # Default return
     if text == '': return ''
     # Creating objects to analyse and translate
@@ -13,16 +16,28 @@ def analyseText(text : str = ''):
     translator = Translator()
 
     # Language detection
-    language = translator.detect(text).lang
-    if language != 'en':
-        # Translating detected language to english
-        translated_text = translator.translate(text, src=language, dest='en').text
-    else:
-        translated_text = text
+    try:
+        language = translator.detect(text).lang
+    except: return 1
+
+    # Skip non english texts
+    if skip_non_eng and language != 'en': return 0
+
+    # Skipping this part if not translating -> better performance
+    if translate:
+        if language != 'en':
+            # Translating detected language to english
+            try:
+                text = translator.translate(text, src=language, dest='en').text
+            except:
+                return 2
         
     # Sentiment analysis
-    scores = sia.polarity_scores(translated_text)
+    scores = sia.polarity_scores(text)
     sentiment = 'Positive' if scores['compound'] > 0 else 'Negative' if scores['compound'] < 0 else 'Neutral'
 
     # Printing results -- only for testing
-    return f"\nTranslated text: {translated_text}\nSentiment: {sentiment}, Scores: {scores}\n"
+    return f"\nAnalysed text: {text}\nSentiment: {sentiment}, Scores: {scores}\n"
+
+
+# print(analyseText(str(input('enter text: ')), False, True))
